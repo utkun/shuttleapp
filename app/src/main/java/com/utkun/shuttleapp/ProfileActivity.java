da68fcc1af3b3113658093fb334b2e12088babc4
Kutlay Hanli <kutlayhanli@yahoo.com>
2017-05-31 22:59:10 +0300

    Navigation Drawer working properly (opening and closing)

--- app/src/main/java/com/utkun/shuttleapp/ProfileActivity.java
+++ app/src/main/java/com/utkun/shuttleapp/ProfileActivity.java
@@ -6,9 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,6 +23,7 @@ public class ProfileActivity extends Activity
 	private String[] titles;
 	private ListView drawerList;
 	private DrawerLayout drawerLayout;
+	private ActionBarDrawerToggle drawerToggle;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -31,6 +35,37 @@ public class ProfileActivity extends Activity
 		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1, titles));
 		drawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
+		//Create the ActionBarDrawerToggle
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
+												 R.string.open_drawer, R.string.close_drawer) {
+			//Called when a drawer has settled in a completely closed state
+			@Override
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+				invalidateOptionsMenu();
+			}
+			
+			//Called when a drawer has settled in a completely open state.
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				invalidateOptionsMenu();
+			}
+		};
+		drawerLayout.setDrawerListener(drawerToggle);
+		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setDisplayShowHomeEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		drawerToggle.syncState();
 	}
 	
 	private class DrawerItemClickListener implements ListView.OnItemClickListener
@@ -59,5 +94,30 @@ public class ProfileActivity extends Activity
 				
 		}
 		Log.d("Position", Integer.toString(position));
+		drawerLayout.closeDrawer(drawerList);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		getMenuInflater().inflate(R.menu.menu_main,menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		if (drawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		switch (item.getItemId())
+		{
+			case R.id.action_settings:
+				Log.d("Menu","working");
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+		
 	}
 }
